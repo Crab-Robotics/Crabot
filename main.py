1c8b5fa40df4689ada15244b111af479e9dd3429
GK <jpkutner+github@gmail.com>
2020-08-23 12:03:27 -0500

    Fixed launch config

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
