6e9bca9d4ff16a94c24e0ca737b30faeff871be6
GK <jpkutner+github@gmail.com>
2020-08-30 13:09:02 -0500

    Replaced the line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
