2829c61b2b483c1833db7f63760515347dabcfb6
Kate Whited <galaxygirl418@icloud.com>
2020-11-07 18:21:33 -0600

    Added Skills

--- KateSkillsLibrary.py
+++ KateSkillsLibrary.py
@@ -9,10 +9,20 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import CrabRobotLibrary
 
+
 # define class robot
 class Kate_Skills:
+
+    color_sensor_left = ColorSensor(Port.S3)
+    color_sensor_right = ColorSensor(Port.S4)
+    
     def __init__(self, robot):
         self.robot=robot
 
     def tell_me_about_your_skills(self):
-        print("to do")
+        print("Check out these skills from Kate:")
+        print("Left color sensor RGB reading is",self.color_sensor_left.rgb())
+        print("Left color sensor color is",self.color_sensor_left.color())
+        print("Right color sensor RGB reading is",self.color_sensor_right.rgb())
+        print("Right color sensor color is",self.color_sensor_right.color())
+        print("My Gyro is reading",self.robot.gyro.angle())
