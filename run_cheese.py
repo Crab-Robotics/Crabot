f26b44330e496721cd934f154675aecc4adfe5ce
GK <jpkutner+github@gmail.com>
2020-11-08 14:18:07 -0600

    wip

--- run_cheese.py
+++ run_cheese.py
@@ -12,14 +12,17 @@ import CrabRobotLibrary
 def run_table(crabot):
 	crabot.gyro_reset()
 	crabot.gyro_drive(300, 0, 1300)
-	crabot.gyro_drive(40, 0, 350)
+	crabot.gyro_drive(40, 0, 365)
 	crabot.bw_gyro_drive(-150, 0, -15)
 	crabot.gyro_turn(-90, -90)
 	crabot.gyro_reset()
-	crabot.bw_gyro_drive(-80, 0, -30)
+	crabot.bw_gyro_drive(-80, 0, -40)
 	crabot.gyro_reset()
 	crabot.gyro_drive(160, -0, 1000)
-	
+	crabot.gyro_turn(25, Direction.CLOCKWISE)
+	crabot.gyro_reset()
+	crabot.gyro_drive(90, 0, 140)
+
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
 	run_table(crabot)
