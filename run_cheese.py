c45beecb32bf4ba28fb0deb27399d96164553528
GK <jpkutner+github@gmail.com>
2020-11-15 14:41:54 -0600

    wip

--- run_cheese.py
+++ run_cheese.py
@@ -15,6 +15,7 @@ def run_table(crabot):
 
 	# todo: lower the front medium motor so its down
 
+	crabot.front_activate(100, 65, Direction.COUNTERCLOCKWISE)
 	crabot.gyro_drive(300, 0, 1300)
 	crabot.gyro_drive(40, 0, 365)
 	crabot.bw_gyro_drive(-150, 0, -15)
@@ -25,10 +26,9 @@ def run_table(crabot):
 
 	crabot.bw_gyro_drive(-80, 0, -40)
 	crabot.gyro_reset()
-	crabot.gyro_drive(160, -0, 1000)
+	crabot.gyro_drive(300, -0, 1000)
 	crabot.gyro_turn(35, Direction.CLOCKWISE)
 	crabot.gyro_reset()
-	crabot.front_activate(100, 65, Direction.COUNTERCLOCKWISE)
 	crabot.gyro_drive(90, 0, 150)
 	wait(1000)
 	crabot.front_activate(100, 50, Direction.COUNTERCLOCKWISE)
@@ -36,7 +36,7 @@ def run_table(crabot):
 	crabot.gyro_turn(-85, Direction.COUNTERCLOCKWISE) 
 	crabot.gyro_reset()
 	crabot.front_activate(100, 50, Direction.CLOCKWISE)
-	crabot.gyro_drive(160, 0, 600)
+	crabot.gyro_drive(250, 0, 600)
 	while True:
 		crabot.gyro_drive(90, 0, 100)
 		crabot.bw_gyro_drive(-90, 0, -100)
@@ -53,10 +53,9 @@ def run_table(crabot):
 		crabot.gyro_turn(45, Direction.CLOCKWISE)
 		crabot.gyro_reset()
 		wait(200)
-		crabot.front_activate(-100, -50, Direction.COUNTERCLOCKWISE)
-		crabot.front_activate(100, 50, Direction.CLOCKWISE)
+		crabot.front_activate(-100, -100, Direction.COUNTERCLOCKWISE)
+		crabot.front_activate(100, 100, Direction.CLOCKWISE)
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
 	run_table(crabot)
-
- 
+	
