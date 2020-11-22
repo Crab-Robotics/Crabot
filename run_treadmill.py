283273880151cf5f0563bebbf4c967527fc443d9
GK <jpkutner+github@gmail.com>
2020-11-22 14:35:56 -0600

    wip

--- run_treadmill.py
+++ run_treadmill.py
@@ -14,9 +14,14 @@ def run_treadmill(crabot):
 	crabot.gyro_drive(400, 0, 1850)
 	crabot.gyro_drive_until_white(200, 0, "left")
 	crabot.gyro_turn(20, Direction.CLOCKWISE)
-	crabot.gyro_drive(100, 20, 300)
+	crabot.gyro_drive(200, 20, 300)
 	crabot.gyro_turn(-20, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_drive(200, 0, 200)
+	crabot.gyro_drive(200, 0, 250)
+	wait(2000)
+	crabot.bw_gyro_drive(-150, 0, -15)
+	crabot.gyro_turn(-90, -90)
+	crabot.gyro_reset()
+	crabot.bw_gyro_drive(-100, 0, -200)
 
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
