dea443f251fb073c80929d48a2af664abb380b95
Kate Whited <galaxygirl418@icloud.com>
2020-11-22 15:03:58 -0600

    added comments and adjusted speed. (need to add braking)

--- run_basketball.py
+++ run_basketball.py
@@ -10,35 +10,45 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import CrabRobotLibrary
 
 def run_table_1(crabot):
-	#This will run the basketball mission.
+	#This pushes the innovation project out(M01).
 	crabot.gyro_reset()
-	crabot.gyro_drive(300, 0, 600)
+	crabot.gyro_drive(500, 0, 600)
 	crabot.bw_gyro_drive(-100, 0, -50)
+
+    #This takes us to the basketball hoop(M05).
+
 	crabot.gyro_turn(45, Direction.CLOCKWISE)
 	crabot.gyro_reset()
-	crabot.gyro_drive(300, 0, 780)
-	crabot.gyro_turn(-85, Direction.COUNTERCLOCKWISE)
+	crabot.gyro_drive(700, 0, 780)
+	crabot.gyro_turn(-90, Direction.COUNTERCLOCKWISE)
 	crabot.gyro_reset()
 	crabot.gyro_drive(500, 0, 390)
 def run_table_2(crabot):
-    #this line of code does the first level of the elevator
+
+    #this code does the first level of the basketball and lifts the red cube(M05,M08).
+
     crabot.front_activate(10000, 5766, Direction.CLOCKWISE)
-    wait(500)
     crabot.front_activate(10000, 1440, Direction.COUNTERCLOCKWISE)
     crabot.gyro_reset()
     crabot.bw_gyro_drive(-100,0,-105)
     crabot.front_activate(10000, 3966, Direction.COUNTERCLOCKWISE)
+
+    #This code does the second level of the basketball(M05).
+
     crabot.gyro_reset()
-    crabot.gyro_drive(300,0,130)
+    crabot.gyro_drive(300,0,100)
     crabot.front_activate(10000, 11440, Direction.CLOCKWISE)
     crabot.front_activate(10000, 1490, Direction.COUNTERCLOCKWISE)
     crabot.gyro_reset()
+
+    #This code knocks over the bench and returns home(M04).
+    
     crabot.bw_gyro_drive(-500,0,-450)
     crabot.gyro_reset()
     crabot.gyro_turn(-27, Direction.COUNTERCLOCKWISE)
     crabot.gyro_reset()
     crabot.gyro_drive(1000, 0, 1300)
-    crabot.front_activate(10000, 8000, Direction.COUNTERCLOCKWISE)
+    crabot.front_activate(10000, 9230, Direction.COUNTERCLOCKWISE)
     crabot.gyro_reset()
 	
 if __name__ == '__main__':
