7f5aa1143ea49b1d065e37f2882b0ba0b8f0270b
GK <jpkutner+github@gmail.com>
2020-11-28 19:24:00 -0600

    I changed run_treadmill.py so I don't need an attachment.

--- run_treadmill.py
+++ run_treadmill.py
@@ -11,17 +11,22 @@ import CrabRobotLibrary
 
 def run_treadmill(crabot):
 	crabot.gyro_reset()
-	crabot.gyro_drive(400, 0, 1850)
-	crabot.gyro_drive_until_white(200, 0, "left")
-	crabot.gyro_turn(20, Direction.CLOCKWISE)
-	crabot.gyro_drive(200, 20, 300)
-	crabot.gyro_turn(-20, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_drive(200, 0, 250)
-	wait(2000)
-	crabot.bw_gyro_drive(-150, 0, -15)
+	crabot.gyro_drive(400, 0, 3000)
+	while crabot.gyro.angle() > 0:
+		crabot.left_wheel.run(-100)
+	crabot.left_wheel.stop()
+	wait(1000)
+	crabot.right_wheel.run_time(800, 3000)
+	# crabot.gyro_drive_until_white(200, 0, "left")
+	# crabot.gyro_turn(20, Direction.CLOCKWISE)
+	# crabot.gyro_drive(200, 20, 300)
+	# crabot.gyro_turn(-20, Direction.COUNTERCLOCKWISE)
+	# crabot.gyro_drive(200, 0, 250)
+	# wait(2000)
+	crabot.bw_gyro_drive(-150, 0, -300)
 	crabot.gyro_turn(-90, -90)
 	crabot.gyro_reset()
-	crabot.bw_gyro_drive(-100, 0, -200)
+	crabot.bw_gyro_drive(-150, 0, -250)
 
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
