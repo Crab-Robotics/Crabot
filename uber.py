0b48dddde76a9a0fa8929024c78ee37d038d95ba
GK <jpkutner+github@gmail.com>
2020-12-12 13:13:44 -0600

    Put programs into uber.py

--- uber.py
+++ uber.py
@@ -8,16 +8,19 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import CrabRobotLibrary
+import run_cheese
+import run_treadmill
+import run_basketball
 
-#def mission1():
-	# do mission 1
-	# gyro-drive and stuff
-	# mission 1 is complete
+crabot = CrabRobotLibrary.Robot("Crabot","Ubertastic")
 
 while True:
 	b = EV3Brick.buttons.pressed()
 	if Button.CENTER in b:
-		print("The center button was pressed.")
+		print("Running run_treadmill.")
+		run_treadmill.go_there(crabot)
+		run_treadmill.row_machine(crabot)
+		run_treadmill.treadmill(crabot)
 		wait(2000)
 	elif Button.LEFT in b:
 		print("The left button was pressed.")
@@ -26,10 +29,15 @@ while True:
 		print("The right button was pressed.")
 		wait(2000)
 	elif Button.UP in b:
-		 #M01, M04, M05, M08 (red or blue blocks)
+		#M01, M04, M05, M08 (red or blue blocks)
+		print("Running run_basketball.")
+		run_basketball.run_table_1(crabot)
+		run_basketball.run_table_2(crabot)
 		wait(2000)
 	elif Button.DOWN in b:
 		# M02, M06, M07, M08 (yellow block)
+		print("Running run_cheese.")
+		run_cheese.run_table(crabot)
 		wait(2000)
 	else:
 		wait(10)
