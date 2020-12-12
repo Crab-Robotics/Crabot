7090049e750565750f060914692452f3ce4ad81c
GK <jpkutner+github@gmail.com>
2020-12-12 13:13:15 -0600

    Tweaked run_treadmill

--- run_treadmill.py
+++ run_treadmill.py
@@ -9,28 +9,13 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 import CrabRobotLibrary
 
-def run_treadmill(crabot):
+def go_there(crabot):
 	crabot.gyro_reset()
 	crabot.gyro_drive(800, 0, 2200)
 	crabot.gyro_drive_until_white(400, 0, "left")
 	crabot.gyro_drive(100, 0, 165)
 	crabot.gyro_turn(-90, Direction.COUNTERCLOCKWISE)
 	crabot.bw_gyro_drive(-100, -90, -300)
-	# while crabot.gyro.angle() > 0:
-	# 	crabot.left_wheel.run(-100)
-	# crabot.left_wheel.stop()
-	# wait(1000)
-	# crabot.right_wheel.run_time(800, 3000)
-	# crabot.gyro_drive_until_white(200, 0, "left")
-	# crabot.gyro_turn(20, Direction.CLOCKWISE)
-	# crabot.gyro_drive(200, 20, 300)
-	# crabot.gyro_turn(-20, Direction.COUNTERCLOCKWISE)
-	# crabot.gyro_drive(200, 0, 250)
-	# wait(2000)
-	# crabot.bw_gyro_drive(-150, 0, -300)
-	# crabot.gyro_turn(-90, -90)
-	# crabot.gyro_reset()
-	# crabot.bw_gyro_drive(-150, 0, -250)
 
 def treadmill(crabot):
 	print("time to get those steps in!")
@@ -50,19 +35,19 @@ def row_machine(crabot):
 	print("row row row your boat")
 	crabot.gyro_reset()
 	crabot.front_reset(Direction.COUNTERCLOCKWISE)
-	crabot.bw_gyro_drive(-20, 0, -20)
+	# crabot.bw_gyro_drive(-20, 0, -20)
 	crabot.gyro_drive(200, 0, 430)
 	crabot.gyro_turn(50, Direction.CLOCKWISE)
-	crabot.gyro_drive(100, 55, 20)
-	crabot.front_activate(100, 220, Direction.CLOCKWISE)
+	crabot.gyro_drive(100, 55, 60)
+	crabot.front_activate(100, 240, Direction.CLOCKWISE)
 	crabot.front_dog_gear.brake()
 	crabot.gyro_turn(0, Direction.COUNTERCLOCKWISE)
 	crabot.bw_gyro_drive(-100, 0, -50)
 	crabot.front_reset(Direction.COUNTERCLOCKWISE)
-	crabot.bw_gyro_drive(-100, 0, -400)
+	crabot.bw_gyro_drive(-250, 0, -500)
 
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
-	run_treadmill(crabot)
+	go_there(crabot)
 	row_machine(crabot)
 	treadmill(crabot)
