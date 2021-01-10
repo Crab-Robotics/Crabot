e5ea0e0e038670e6eaaacd186755cce30e505fdd
Blastoff88 <71273716+Blastoff88@users.noreply.github.com>
2021-01-10 13:51:47 -0600

    Added more degrees to the front activate before the backup.
    This makes it stall less.

--- run_basketball.py
+++ run_basketball.py
@@ -2,7 +2,7 @@
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+								 InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -12,50 +12,40 @@ import CrabRobotLibrary
 def run_table_1(crabot):
 	#This pushes the innovation project out(M01).
 	crabot.gyro_reset()
-	crabot.bw_gyro_drive(-500, 0, -10)
-	crabot.gyro_reset()
-	crabot.gyro_drive(250, 0, 570)
+	crabot.gyro_drive(500, 0, 590)
 	crabot.brake()
-	wait(250)
 	crabot.bw_gyro_drive(-100, 0, -50)
-	wait(250)
 
-    #This takes us to the basketball hoop(M05).
+	#This takes us to the basketball hoop(M05).
 
 	crabot.gyro_turn(45, Direction.CLOCKWISE)
-	crabot.gyro_reset()
-	crabot.gyro_drive(250, 0, 750)
+	crabot.gyro_drive(500, 45, 735)
 	crabot.brake()
-	crabot.gyro_turn(-90, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_reset()
-	crabot.gyro_drive(500, 0, 450)
+	crabot.gyro_turn(-40, Direction.COUNTERCLOCKWISE)
+	crabot.brake()
+	crabot.gyro_drive(500, -45, 390)
 def run_table_2(crabot):
 
-# 	#this code does the first level of the basketball and lifts the red cube(M05,M08).
+	#this code does the first level of the basketball and lifts the red cube(M05,M08).
 
 	crabot.front_activate(10000, 5766, Direction.CLOCKWISE)
-	crabot.front_activate(10000, 1440, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_reset()
-	crabot.bw_gyro_drive(-100,0,-105)
-	crabot.front_activate(10000, 3966, Direction.COUNTERCLOCKWISE)
+	crabot.front_activate(10000, 1800, Direction.COUNTERCLOCKWISE)
+	crabot.bw_gyro_drive(-100,-45,-105)
+	crabot.front_activate(10000, 3786, Direction.COUNTERCLOCKWISE)
 
-# 	#This code does the second level of the basketball(M05).
+	#This code does the second level of the basketball(M05).
 
-	crabot.gyro_reset()
-	crabot.gyro_drive(300,0,80)
+	crabot.gyro_drive(300,-45,100)
 	crabot.front_activate(10000, 11440, Direction.CLOCKWISE)
 	crabot.front_activate(10000, 1490, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_reset()
 
-# 	#This code knocks over the bench and returns home(M04).
-
-	crabot.bw_gyro_drive(-500,0,-450)
-	crabot.gyro_reset()
-	crabot.gyro_turn(-27, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_reset()
-	crabot.gyro_drive(1000, 0, 1300)
-	crabot.front_activate(10000, 9230, Direction.COUNTERCLOCKWISE)
-	crabot.gyro_reset()
+	#This code knocks over the bench and returns home(M04).
+	
+	crabot.front_forever(10000, Direction.COUNTERCLOCKWISE)
+	crabot.bw_gyro_drive(-500,-45,-435)
+	crabot.gyro_turn(-75, Direction.COUNTERCLOCKWISE)
+	crabot.gyro_drive(1000, -75, 1350)
+	crabot.reset_elevator()
 	
 if __name__ == '__main__':
 	crabot = CrabRobotLibrary.Robot("Crabot","Competitive")
