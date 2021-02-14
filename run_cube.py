3a45bbdc9bfdb7a8f60c654721e13e4a46ce7086
GK <jpkutner+github@gmail.com>
2021-02-14 11:12:20 -0600

    Made run_cube back up further

--- run_cube.py
+++ run_cube.py
@@ -2,7 +2,7 @@
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+								 InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -10,20 +10,19 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import CrabRobotLibrary
 
 def run_table_3(crabot):
-    #drives to the big boccia ball area
-    crabot.gyro_reset()
-    crabot.gyro_drive(500, 0, 130)
-    crabot.gyro_turn(48, Direction.CLOCKWISE)
-    crabot.gyro_drive(10000, 48, 1650)
-    crabot.brake()
-    #Dumps out the cubes
-    crabot.gyro_reset()
-    crabot.front_activate(10000, 3600, Direction.CLOCKWISE)
-    #comes back to home
-    crabot.bw_gyro_drive(-10000, 0, -1710)
-    crabot.brake()
-    crabot.reset_elevator()
+	#drives to the big boccia ball area
+	crabot.gyro_reset()
+	crabot.gyro_drive(500, 0, 130)
+	crabot.gyro_turn(48, Direction.CLOCKWISE)
+	crabot.gyro_drive(10000, 48, 1650)
+	crabot.brake()
+	#Dumps out the cubes
+	crabot.front_activate(10000, 3600, Direction.CLOCKWISE)
+	#comes back to home
+	crabot.bw_gyro_drive(-10000, 48, -1780)
+	crabot.brake()
+	crabot.reset_elevator()
 
 if __name__ == '__main__':
-    crabot = CrabRobotLibrary.Robot('Crabot','Competitive')
-    run_table_3(crabot)
+	crabot = CrabRobotLibrary.Robot('Crabot','Competitive')
+	run_table_3(crabot)
